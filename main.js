190d12cf3a0b37c6f2a0dc66fcee90cc5762b2cc
AlbertLeipzig <albertclemvill@gmail.com>
2025-01-29 21:20:48 +0100

    wip

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-import "./animations/animations.js";
+/* import "./animations/animations.js"; */
+const testParent = document.querySelector(".test-parent");
+
+const testChild = () => {
+  const element = document.createElement("div");
+  element.className = "test-child";
+  return element;
+};
+
+const line = (direction) => {
+  const element = document.createElement("div");
+  element.className = `test-${direction}-line`;
+
+  return element;
+};
+
+Array.from({ length: 1000 }).forEach((_, i) => {
+  testParent.appendChild(line("vertical"));
+  testParent.appendChild(line("horizontal"));
+});
